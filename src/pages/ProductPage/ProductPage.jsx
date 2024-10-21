dea444a296db58f4b04c1039354d849285c359ee
Negin Bandar <bandar.negin@gmail.com>
2024-10-21 18:00:14 -0400

    creating product page components

--- src/pages/ProductPage/ProductPage.jsx
+++ src/pages/ProductPage/ProductPage.jsx
@@ -1,4 +1,6 @@
 import productPicture from "../../assets/images/_.jpeg";
+import "./ProductPage.scss";
+import heartIcon from "../../assets/icons/heart.svg";
 
 export default function ProductPage() {
   return (
@@ -8,10 +10,18 @@ export default function ProductPage() {
         src={productPicture}
         alt="product-picture"
       />
-      <div className="product-list">
-        <h2 className="product-list__title">Shop Products</h2>
-        <div className="product-list__thumbnail">
+      <h2 className="product-list__title">Shop Products</h2>
+      <div className="product-card">
+        <div className="product-card__thumbnail">
           <img src={productPicture} alt="product-thumbnail" />
+          <div className="product-card">
+            <h5>Product Name</h5>
+            <p>Product Description</p>
+            <div className="button-group">
+              <button>Shop Now</button>
+              <img className="icon-small" src={heartIcon} alt="heart-icon" />
+            </div>
+          </div>
         </div>
       </div>
     </div>
